2c76abc5e04b01c5c1b059c2a582884cdb03bd9d
mr-chidex <mbahchiboy2@gmail.com>
2021-03-18 13:31:36 +0100

    default login details for admin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
 
+const User = require("./models/user");
 const userRoutes = require("./routes/user");
 const imageRoutes = require("./routes/image");
 
@@ -29,6 +30,22 @@ mongoose
     app.listen(process.env.PORT || 5000, () =>
       console.log("server running...")
     );
+  })
+  .then(async () => {
+    //create default admin login details if admin is not in database
+    const user = await User.find();
+    if (!user.length > 0) {
+      const admin = await new User({
+        name: process.env.ADMIN_NAME,
+        email: process.env.ADMIN_EMAIL,
+        password: process.env.ADMIN_PASS,
+        isAdmin: true,
+      });
+
+      await admin.save();
+
+      console.log("Admin succesfully added");
+    }
   })
   .catch((err) => {
     console.log(err);
--- models/user.js
+++ models/user.js
@@ -24,6 +24,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
